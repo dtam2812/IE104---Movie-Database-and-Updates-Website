b66986078182d1f03d8af2cb09ec35d06d175ddd
thinh1311ss <pthinhn.com>
2025-10-17 01:31:17 +0700

    Thêm modal đăng nhập/đăng ký trong header - Xử lý sự kiện mở/đóng/chuyển modal - Xử lý sự kiện đăng nhập/đăng xuất (* khi reload trang thì phải đăng nhập lại) - Cập nhật nút thành viên thành avatar sau khi đăng nhập - Thêm dropdown-menu cho user - Cập nhật thêm các trang chính sách

--- js/Header.js
+++ js/Header.js
@@ -7,7 +7,7 @@ const searchNav = document.querySelector('.search-toggle');
 const searchBox = document.querySelector('.search');
 const logo = document.querySelector('.header-logo');
 const dropdown = document.querySelector('.menu-film-type.dropdown');
-const dropdownBtn = document.querySelector('.dropdown-toggle')
+const dropdownBtn = dropdown.querySelector('.dropdown-toggle');
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('toggled');
@@ -23,6 +23,13 @@ searchNav.addEventListener('click', () => {
 
 dropdownBtn.addEventListener('click', () => {
   dropdown.classList.toggle('toggled');
+});
+
+document.addEventListener('click', (e) =>
+{
+  if(!dropdown.contains(e.target)){
+    dropdown.classList.remove('toggled')
+  }
 });
 
 window.addEventListener('scroll', () => {
@@ -41,11 +48,12 @@ if (memberBtn) {
         e.preventDefault();
         let modal = document.querySelector('.modal');
         if (!modal) {
+            //load file AuthModal.html vào header.html
             const html = await (await fetch('../../view/components/AuthModal.html')).text();
             const doc = new DOMParser().parseFromString(html, 'text/html');
             document.body.appendChild(doc.querySelector('.modal'));
 
-            // Load CSS
+            // Load file AuthModal.css vào header.html
             doc.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
                 const href = link.href;
                 if (!document.querySelector(`link[href="${href}"]`)) {
@@ -53,7 +61,7 @@ if (memberBtn) {
                 }
             });
 
-            // Fix import fiel 
+            // import filr AuthModal.js vào 
             const { Auth_Modaljs } = await import('./AuthModal.js'); 
             Auth_Modaljs();
             setTimeout(() => window.openLRFModal('login'), 50);
@@ -63,6 +71,7 @@ if (memberBtn) {
     });
 }
 
+  //Xử lý sự kiện sau khi user đăng nhập vào
   document.addEventListener('userLoggedIn', (e) => {
   const guest = document.getElementById('user_guest');
   const logged = document.getElementById('main_user');
@@ -72,5 +81,40 @@ if (memberBtn) {
       logged.classList.remove('hidden');
   }
   });
+
+  //Xử lý sự kiện khi click vào main user ở heder
+  const userDropdownMenu = document.querySelector('.user-dropdown-menu');
+  const dropdownList = userDropdownMenu.querySelector('.dropdown-list');
+  
+  userDropdownMenu.addEventListener('click', (e) => {  
+     dropdownList.classList.toggle('show')
+  })
+  
+  //tắt user-dropdown-menu khi click ở bên ngoài menu
+  document.addEventListener('click', (e) => {
+  if(!userDropdownMenu.contains(e.target)){
+    dropdownList.classList.remove('show')}
+  });
+
+
+   //Xử lý sự kiện đăng xuất
+  const logOutBtn = dropdownList.querySelector('#Log-out-Btn');
+
+  if (logOutBtn) 
+  {
+  logOutBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const guest = document.getElementById('user_guest');
+  const logged = document.getElementById('main_user');
+
+  if (guest && logged) 
+    {
+      logged.classList.add('hidden');
+      guest.classList.remove('hidden');
+    }
+
+  });
+  }
 
 } 
